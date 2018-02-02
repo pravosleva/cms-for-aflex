52be001d555e9c555fcb5ada585b503824080310
pravosleva <selection4test@gmail.com>
2018-02-02 22:11:17 +0300

    Updates
    - dom has been restructed
    TODO:
    - chatHistory like as [str. str, str]
    - chatHistory could be showed in dialog modal

--- routes/views/clients.js
+++ routes/views/clients.js
@@ -5,6 +5,7 @@ exports = module.exports = function (req, res) {
   var locals = res.locals;
 
   locals.section = 'store';
+  locals.chatHistoryExample = ['[msg] A: Hello.', '[msg] B: Hi.', '[msg] A: Wsp?'];
 
   view.query( 'clients', keystone.list('Client').model.find() );
 
